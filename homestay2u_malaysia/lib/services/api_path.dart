f4df8ec580352c4ad98cd85d0d3afe328b97ab13
Naszrul-NZL <muhdnaszrul17@gmail.com>
2026-06-08 17:47:04 +0800

    Add ApiPath

---
+++ homestay2u_malaysia/lib/services/api_path.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:homestay2u_malaysia/models/homestay.dart';
+class ApiPath {
+
+  static String get baseUrl {
+
+    if(kIsWeb){
+      return "http://slum78.myddns.me/homestay2u/api";
+
+    }
+    switch (defaultTargetPlatform){
+      case TargetPlatform.android:
+        return "http://slum78.myddns.me/homestay2u/api";
+      default:
+        return "http://slum78.myddns.me/homestay2u/api";
+
+    }
+
+  } 
+
+  static String endpoint (String path) {
+    return "$baseUrl/$path";
+
+  }
+
+  static String get homestays => endpoint("homestays");
+  static String get states => endpoint("states");
+  static String searchHomestays (String keyword) => endpoint ("homestays?state=$state");
+  static String filterByStateAndDistrict (String state, String district) => endpoint ("homestays?state=$state&district=$district");
+
+}
